a69347a776b3ba9fb7bb103b8f8a2aa09af7dc7d
arienungky <85285853+arienungky@users.noreply.github.com>
2024-04-04 01:06:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-54e9b530-de50-419b-9cf4-a4f3f4939507/)
 
 
 echo "$curl_output" >> Update.tsx
